89e643fe6b77775f67ad192867418f6b93704632
AlieksandrDanil <inspcrimea@gmail.com>
2022-05-04 11:46:01 +0300

    Branch feat(mvvm)-HW-Task2.2

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,8 +5,8 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likes: Int = 0,
-    var likedByMe: Boolean = false,
-    var shared: Int = 0,
-    var viewed: Int = 0
+    val likes: Int = 0,
+    val likedByMe: Boolean = false,
+    val shared: Int = 0,
+    val viewed: Int = 0
 )
